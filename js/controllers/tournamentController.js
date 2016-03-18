3acc836976d0443119ce6e62dba2e1ffe21013e0
architMahto <architmahto@cmail.carleton.ca>
2016-03-18 16:24:06 -0600

    Added Euro 2016 and Champions League images. Added json files for tournaments and countries. Added call to consume tournaments.json.

--- js/controllers/tournamentController.js
+++ js/controllers/tournamentController.js
@@ -5,9 +5,22 @@
     .controller('tournamentController', tournamentController)
 
   // Controller Callback
-  function tournamentController() {
+  function tournamentController($http) {
     var tournamentCtrl = this;
+    tournamentCtrl.tournaments = [];
 
-    tournamentCtrl.tournaments = ['UEFA Champions League', 'UEFA European Championship'];
+    $http.get("../json/tournaments.json").then(gotTournaments);
+
+    function gotTournaments(response) {
+      response.data.tournaments.forEach(function (element) {
+        tournamentCtrl.tournaments.push(new Tournament(element[0], element[1]));
+      });
+      console.log(tournamentCtrl.tournaments);
+    }
+
+    function Tournament(name, imageURL) {
+      this.name = name;
+      this.imageURL = imageURL;
+    }
   }
 })();
