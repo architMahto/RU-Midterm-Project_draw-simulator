0bbcfaba8a1e2f324f10e6b0e41187d75208db3c
architMahto <architmahto@cmail.carleton.ca>
2016-03-21 21:56:25 -0600

    Added clubs of countries starting with A and B to countries.json. Added functionality to add clubs to each country.

--- js/controllers/selectController.js
+++ js/controllers/selectController.js
@@ -12,17 +12,32 @@
     $http.get("../json/countries.json").then(getCountries);
 
     function getCountries(response) {
-      // console.log(response.data);
-      console.log(response.data);
+
       response.data.countries.forEach(function(element) {
         selectCtrl.countries.push(new Country(element[0], element[1]));
       });
-      console.log(selectCtrl.countries);
+
+      // for (var i = 0; i < response.data.clubs.length; i++) {
+      //   response.data.clubs[i].forEach(function(element) {
+      //     selectCtrl.countries[i].clubs.push(new Club(element[0], element[1], selectCtrl.countries[i].name));
+      //   });
+      // }
+
+      // console.log(selectCtrl.countries);
     }
 
+    // Country constructor function
     function Country(name, imageURL) {
       this.name = name;
       this.imageURL = imageURL;
+      this.clubs = [];
+    }
+
+    // Club constructor function
+    function Club(name, city, country) {
+      this.name = name;
+      this.city = city;
+      this.country = country;
     }
   }
 })();
